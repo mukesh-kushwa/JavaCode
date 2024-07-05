61888c69689095deddf3d889cbdf12ef356410c2
mukesh-kushwa <mkkushwah2024@gmail.com>
2024-07-05 07:55:21 +0530

    interface method changes

--- src/com/winterbe/java8/samples/lambda/Interface.java
+++ src/com/winterbe/java8/samples/lambda/Interface.java
@@ -14,7 +14,7 @@ public class Interface {
         }
     }
 
-    public static void main(String[] args) {
+    public static void  main(String[] args) {
         Formula formula1 = new Formula() {
             @Override
             public double calculate(int a) {
